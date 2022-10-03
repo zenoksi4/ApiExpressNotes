886f41d030f6f69e23d8a5b3b31112dfb7672a6f
Pasha <zenoken4ik@gmail.com>
2022-10-03 16:51:30 +0300

    update structure

--- src/routes/NotesRout.ts
+++ src/routes/NotesRout.ts
@@ -1,16 +1,16 @@
 import express from 'express';
 import { ValidateYup, SchemasCreate, SchemasUpdate } from '../helpers/middleware/ValidateRequest';
-import services from '../services/NotesService'
+import controllers from '../controllers/NotesController'
 
 
 const router = express.Router();
 
 
-router.get('/', services.readAllNotes);
-router.get('/stats', services.readNotesStats);
-router.get('/:id', services.readNote);
-router.post('/', ValidateYup(SchemasCreate.data), services.createNote);
-router.delete('/:id', services.deleteNote);
-router.patch('/:id', ValidateYup(SchemasUpdate.data), services.updateNote)
+router.get('/', controllers.readAllNotes);
+router.get('/stats', controllers.readNotesStats);
+router.get('/:id', controllers.readNote);
+router.post('/', ValidateYup(SchemasCreate.data), controllers.createNote);
+router.delete('/:id', controllers.deleteNote);
+router.patch('/:id', ValidateYup(SchemasUpdate.data), controllers.updateNote)
 
 export = router;
